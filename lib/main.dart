d95eb66ed719ee547b1b5b4eea145fb1981e377f
Hanzala Saeed <hanzalasaeed47@gmail.com>
2021-03-10 16:44:21 +0500

    Bottom Navigation Bar completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/Data/CategorysData.dart';
 import 'package:food_app/Screens/CategoriesScreens/CategoryListScreen.dart';
+import 'package:food_app/Screens/Navigation/NavigationScreen.dart';
 import 'package:food_app/Screens/Popular%20Food/PopularFood.dart';
 import 'package:food_app/Screens/ProfileScreens/ProfileScree.dart';
 import 'package:food_app/Styles/StyleConstants.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: themeData,
       //TO be change to popular food
-      home: ProfileScreen(),
+      home: NavigationScreen(),
     );
   }
 }
