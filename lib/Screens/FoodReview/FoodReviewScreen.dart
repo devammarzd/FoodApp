03726281b74201795f69502f050cb3debef80522
Hanzala Saeed <hanzalasaeed47@gmail.com>
2021-03-15 17:49:34 +0500

    removed glow from food review screen

--- lib/Screens/FoodReview/FoodReviewScreen.dart
+++ lib/Screens/FoodReview/FoodReviewScreen.dart
@@ -31,96 +31,102 @@ class _FoodReviewState extends State<FoodReview> {
       ),
       body: Padding(
         padding: paddingFromScreenBorder,
-        child: SingleChildScrollView(
-          primary: false,
-          child: Container(
-            height: sizeHeight(context) / 1.18,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: Container(
-                    width: sizeWidth(context) / 1.8,
+        child: NotificationListener<OverscrollIndicatorNotification>(
+          onNotification: (OverscrollIndicatorNotification overscroll) {
+            overscroll.disallowGlow();
+            return;
+          },
+          child: SingleChildScrollView(
+            primary: false,
+            child: Container(
+              height: sizeHeight(context) / 1.18,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Container(
+                      width: sizeWidth(context) / 1.8,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "How Was The Food Taste",
+                        style: Theme.of(context).textTheme.headline2,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 45),
+                  Image.asset(
+                    "assets/images/illustrations/Group 34396.png",
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
                     alignment: Alignment.center,
-                    child: Text(
-                      "How Was The Food Taste",
-                      style: Theme.of(context).textTheme.headline2,
-                      textAlign: TextAlign.center,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    height: 50,
+                    width: sizeWidth(context) / 1.6,
+                    child: RatingBar(
+                      rating: 2,
                     ),
                   ),
-                ),
-                SizedBox(height: 45),
-                Image.asset(
-                  "assets/images/illustrations/Group 34396.png",
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10),
+                  SizedBox(
+                    height: 20,
                   ),
-                  height: 50,
-                  width: sizeWidth(context) / 1.6,
-                  child: RatingBar(
-                    rating: 2,
+                  Expanded(
+                    child: Container(),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.comment,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "Add a comment",
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: sizeHeight(context) / 4.5,
-                  child: Stack(
+                  Row(
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.grey[200],
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 10, right: 10),
-                          child: TextField(
-                            maxLines: 5,
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
+                      Icon(
+                        Icons.comment,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Add a comment",
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: sizeHeight(context) / 4.5,
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.grey[200],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10, right: 10),
+                            child: TextField(
+                              maxLines: 5,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Positioned(
-                        left: 5,
-                        bottom: 10,
-                        child: CustomLargeButton(
-                          buttonText: "Done",
-                          onPress: () {},
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                        Positioned(
+                          left: 5,
+                          bottom: 10,
+                          child: CustomLargeButton(
+                            buttonText: "Done",
+                            onPress: () {},
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
