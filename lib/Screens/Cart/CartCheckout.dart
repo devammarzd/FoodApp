2d1664ef793d7a09305b9faa09a6cfb969faa573
Hanzala Saeed <hanzalasaeed47@gmail.com>
2021-03-17 10:23:06 +0500

    fixed responsiveness of cart checkout]

--- lib/Screens/Cart/CartCheckout.dart
+++ lib/Screens/Cart/CartCheckout.dart
@@ -26,110 +26,121 @@ class _CartCheckoutScreenState extends State<CartCheckoutScreen> {
           children: [
             Padding(
               padding: paddingFromScreenBorder,
-              child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.fromLTRB(2, 12, 2, 12),
-                    child: Material(
-                      elevation: 2,
-                      borderRadius: BorderRadius.circular(15),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        width: sizeWidth(context),
-                        height: 100,
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(right: 15),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(15),
-                                child: Image.asset(
-                                  "assets/images/freshorangesjuice.jpg",
-                                  height: 100,
-                                  fit: BoxFit.cover,
+              child: Container(
+                height: sizeHeight(context) / 1.6,
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.fromLTRB(2, 12, 2, 12),
+                      child: Material(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 7,
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          width: sizeWidth(context),
+                          height: 100,
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(right: 15),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(15),
+                                  child: Image.asset(
+                                    "assets/images/freshorangesjuice.jpg",
+                                    height: 100,
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    width: sizeWidth(context) / 2,
-                                    child: Text(
-                                      "Cheesecake with Blueberry",
-                                      style: TextStyle(
-                                        fontSize: 16,
+                              Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      width: sizeWidth(context) / 2,
+                                      child: Text(
+                                        "Cheesecake with Blueberry",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  SizedBox(
-                                    height: 2,
-                                  ),
-                                  Text(
-                                    "Fresh Orange Juice",
-                                    style:
-                                        Theme.of(context).textTheme.headline3,
-                                  ),
-                                  SizedBox(
-                                    height: 5,
-                                  ),
-                                  Container(
-                                    width: sizeWidth(context) / 1.7,
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          "\$05.99",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline1,
-                                        ),
-                                        Container(
-                                          width: sizeWidth(context) / 5,
-                                          decoration: BoxDecoration(
-                                            color: primaryColor,
-                                            borderRadius:
-                                                BorderRadius.circular(30),
-                                          ),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: [
-                                              Icon(
-                                                Icons.remove,
-                                                color: Colors.white,
-                                              ),
-                                              Text(
-                                                "4",
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                              ),
-                                              Icon(
-                                                Icons.add,
-                                                color: Colors.white,
-                                              ),
-                                            ],
-                                          ),
-                                        )
-                                      ],
+                                    SizedBox(
+                                      height: 2,
                                     ),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
+                                    Text(
+                                      "Fresh Orange Juice",
+                                      style:
+                                          Theme.of(context).textTheme.headline3,
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Container(
+                                      width: sizeWidth(context) / 1.7,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            "\$05.99",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headline1,
+                                          ),
+                                          Container(
+                                            width: sizeWidth(context) / 5,
+                                            decoration: BoxDecoration(
+                                              color: primaryColor,
+                                              borderRadius:
+                                                  BorderRadius.circular(30),
+                                            ),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Icon(
+                                                  Icons.remove,
+                                                  color: Colors.white,
+                                                ),
+                                                Text(
+                                                  "4",
+                                                  style: TextStyle(
+                                                      color: Colors.white),
+                                                ),
+                                                Icon(
+                                                  Icons.add,
+                                                  color: Colors.white,
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
             //Checkout details here
